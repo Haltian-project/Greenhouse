bc78d2a2d90204753ec5791178d8cd40127be23f
Bao <tranxbao@gmail.com>
2024-04-13 02:42:28 +0300

    separate backend and frontend

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import forecast from './routers/forecast.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const URI = process.env.MongoDB_URI;
+
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/forecast', forecast);
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
